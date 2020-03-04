88752c9f7c9e252ad329a20f093af97eaff2c0ba
Quang.Tran <quangtd@fimplus.vn>
2020-03-04 18:01:04 +0700

    add config

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,5 +3,5 @@
 sudo docker pull wangtran1990/docker_with_sailsjs
 sudo docker stop docker_with_sailsjs
 sudo docker rm docker_with_sailsjs
-sudo docker run -p 1400:1400 wangtran1990/docker_with_sailsjs:latest docker_with_sailsjs
+sudo docker run -d --name docker_with_sailsjs -p 1400:1400 wangtran1990/docker_with_sailsjs:latest
 echo 'you are all set'
