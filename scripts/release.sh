2f3a60d42c68f1271989a1b2245602a4a5da2a37
Quang.Tran <quangtd@fimplus.vn>
2020-03-04 15:50:40 +0700

    add config

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 # Path needs to be mounted from host to container.
-ls
+sudo docker pull wangtran1990/docker_with_sailsjs
+sudo docker stop docker_with_sailsjs
+sudo docker rm docker_with_sailsjs
+sudo docker run -p 1400:1400 wangtran1990/docker_with_sailsjs:latest docker_with_sailsjs
+echo 'you are all set'
